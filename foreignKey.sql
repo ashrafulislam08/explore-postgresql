8d4331ba4e4c2c1b28346c16e8eb3aa087bd99cb
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-05-21 12:18:03 +0600

    foreign key

---
+++ foreignKey.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE post (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT null,
+    user_id INTEGER REFERENCES "user" (id)
+)

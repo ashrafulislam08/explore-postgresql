e909c9fe16bf8f45168c38fbaddab87ed0821420
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-05-22 12:29:59 +0600

    left and right  join

--- join.sql
+++ join.sql
@@ -9,3 +9,17 @@ FROM post
 SELECT * FROM post as p JOIN "user" on p.user_id = "user".id;
 
 SELECT * FROM post as p INNER JOIN "user" as u on p.user_id = u.id;
+
+INSERT INTO
+    post (id, title, user_id)
+VALUES (
+        5,
+        'This is a test post',
+        NULL
+    );
+
+SELECT * FROM post LEFT JOIN "user" on post.user_id = "user".id;
+
+SELECT *
+FROM post as p
+    RIGHT JOIN "user" as u on p.user_id = u.id;

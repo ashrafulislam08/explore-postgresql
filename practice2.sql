51d25699c2b2ffc2c6342de47d40a30a3a5e5dcc
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-05-24 17:10:30 +0600

    practice2 completed

--- practice2.sql
+++ practice2.sql
@@ -254,3 +254,30 @@ FROM employees
 GROUP BY
     hire_year;
 ;
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2)
+);
+
+-- Inserting sample data into the orders table
+INSERT INTO
+    orders (
+        customer_id,
+        order_date,
+        total_amount
+    )
+VALUES (1, '2022-01-05', 100.50),
+    (2, '2022-01-07', 200.75),
+    (1, '2022-01-08', 150.25),
+    (3, '2022-01-10', 300.00),
+    (2, '2022-01-15', 180.50),
+    (3, '2022-01-20', 220.25),
+    (1, '2022-01-25', 90.00),
+    (2, '2022-01-28', 120.75),
+    (3, '2022-02-01', 250.50),
+    (1, '2022-02-05', 180.25);
+
+SELECT customer_id FROM orders GROUP BY customer_id;
